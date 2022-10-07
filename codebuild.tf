f5b2a5a97fd5b1465d7984daa97d639f8e17a224
martivo <martin.vool@entigo.com>
2022-10-07 10:25:56 +0300

    Update image of codebuild

--- codebuild.tf
+++ codebuild.tf
@@ -127,7 +127,7 @@ resource "aws_codebuild_project" "build" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:2.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
 
