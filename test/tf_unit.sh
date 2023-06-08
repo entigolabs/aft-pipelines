9871691b6d959a960ea42ce6874827765c4b2064
martivo <martin.vool@entigo.com>
2023-06-08 15:05:48 +0300

    Add unit tests

--- test/tf_unit.sh
+++ test/tf_unit.sh
@@ -2,5 +2,4 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/.."
 cd $SCRIPTPATH || exit 1
 
-
-docker run -it --rm -v "$(pwd)":"/app" -w /app/test test go test -v -timeout 30m
+docker run -it --rm -v "$(pwd)":"/app" -w /app  entigolabs/entigo-infralib-testing
